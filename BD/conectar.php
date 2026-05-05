a3009039651787ab0ae465506fa300d66edd233b
angel987ort-design <angel987ort@gmail.com>
2026-05-04 21:44:25 -0600

    Update conectar.php
    Conecte la bd de supabase al proyecto

--- BD/conectar.php
+++ BD/conectar.php
@@ -1,18 +1,19 @@
 <?php
 class Conexion {
     public static function ConexionBD() {
-        $host = "localhost";
-        $dbname = "DocManager";
-        $username = "postgres";
-        $password = "Angelcam78"; 
+
+       $host = "db.rxkxdwknfhqgjwncqhbu.supabase.co";
+$port = "5432";
+$dbname = "postgres";
+$user = "postgres";
+$password = "@DocManager2026";
 
         try {
-            $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
-        } catch (PDOException $exp) {
-            echo ("Error al conectar a la base de datos: $exp");
-            return null;
+            $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
         }
     }
 }
